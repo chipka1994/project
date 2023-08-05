5cc260ef1ec8fa952eeb16921d926b91763a2197
igor <example@gmail.com>
2023-08-05 18:52:50 +0300

    first comit

---
+++ script.js
@@ -0,0 +1,18 @@
+"use scrict";
+//alert('Hello!');
+
+//let resalt = confirm("are you ready?");
+//console.log(resalt);
+
+//const answer = prompt("ваша любимая еда?",);
+//console.log(answer);
+
+let incr = 10,
+    decr = 10;
+incr++;
+decr--;    
+
+console.log(incr);
+console.log(decr);    
+
+
